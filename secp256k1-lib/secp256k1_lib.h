4dada1b6181cbc39d5b71b6a05e09087e899b34f
Amit Shah <amitshah0t7@gmail.com>
2018-05-30 16:59:59 -0500

    Initial Commit

---
+++ secp256k1-lib/secp256k1_lib.h
@@ -0,0 +1,19 @@
+//
+//  secp256k1_lib.h
+//  secp256k1-lib
+//
+//  Created by Amit Shah on 2018-05-30.
+//  Copyright © 2018 Amit Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for secp256k1_lib.
+FOUNDATION_EXPORT double secp256k1_libVersionNumber;
+
+//! Project version string for secp256k1_lib.
+FOUNDATION_EXPORT const unsigned char secp256k1_libVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <secp256k1_lib/PublicHeader.h>
+
+
---
+++ secp256k1-libTests/secp256k1_libTests.m
@@ -0,0 +1,39 @@
+//
+//  secp256k1_libTests.m
+//  secp256k1-libTests
+//
+//  Created by Amit Shah on 2018-05-30.
+//  Copyright © 2018 Amit Shah. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface secp256k1_libTests : XCTestCase
+
+@end
+
+@implementation secp256k1_libTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
