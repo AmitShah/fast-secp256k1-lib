8da5f1995a1b3445a3492da02f612ab22ff11320
Amit Shah <amitshah0t7@gmail.com>
2018-06-01 00:13:44 -0500

    rerrange dependencies so secp256k1 header files are not needed at root level

--- secp256k1-lib/secp256k1_lib.h
+++ secp256k1-lib/secp256k1_lib.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "utility.h"
 
 
 //! Project version number for secp256k1_lib.
--- secp256k1-lib/utility.m
+++ secp256k1-lib/utility.m
@@ -8,7 +8,186 @@
 
 #import <Foundation/Foundation.h>
 #import "utility.h"
+#import "secp256k1.h"
+#import "secp256k1_ecdh.h"
+#import "secp256k1_recovery.h"
+#import "util.h"
+#import "hash_impl.h"
+#import "keccak-tiny.h"
 
+static int custom_nonce_function_rfc6979(unsigned char *nonce32, const unsigned char *msg32, const unsigned char *key32, const unsigned char *algo16, void *data, unsigned int counter){
+    return secp256k1_nonce_function_rfc6979(nonce32, msg32, key32, algo16, data, counter);
+}
+
+static NSData *stripDataZeros(NSData *data) {
+    const char *bytes = data.bytes;
+    NSUInteger offset = 0;
+    while (offset < data.length && bytes[offset] == 0) { offset++; }
+    return [data subdataWithRange:NSMakeRange(offset, data.length - offset)];
+}
+
+//hexBytes expect utf8strin encoded bytes within byte boundary
+static NSData * dataFromChar(const char * chars, int len){
+    
+    int i = 0;
+    bool flag = false;
+    if(len > 1 && len %2!=0){
+        flag = true;
+    }
+    NSMutableData *data = [NSMutableData dataWithCapacity:len / 2];
+    char byteChars[3] = {'\0','\0','\0'};
+    unsigned long wholeByte;
+    
+    while (i < len) {
+        if(flag){
+            byteChars[0] = '0';
+            byteChars[1] = chars[i++];
+            wholeByte = strtoul(byteChars, NULL, 16);
+            [data appendBytes:&wholeByte length:1];
+            flag = false;
+        }
+        else{
+            byteChars[0] = chars[i++];
+            byteChars[1] = chars[i++];
+            wholeByte = strtoul(byteChars, NULL, 16);
+            [data appendBytes:&wholeByte length:1];
+        }
+    }
+    //unsigned char *bytePtr = (unsigned char *)[data bytes];
+    return data;
+    
+}
+
+static int secp256k1_der_read_len(const unsigned char **sigp, const unsigned char *sigend) {
+    int lenleft, b1;
+    size_t ret = 0;
+    if (*sigp >= sigend) {
+        return -1;
+    }
+    b1 = *((*sigp)++);
+    if (b1 == 0xFF) {
+        /* X.690-0207 8.1.3.5.c the value 0xFF shall not be used. */
+        return -1;
+    }
+    if ((b1 & 0x80) == 0) {
+        /* X.690-0207 8.1.3.4 short form length octets */
+        return b1;
+    }
+    if (b1 == 0x80) {
+        /* Indefinite length is not allowed in DER. */
+        return -1;
+    }
+    /* X.690-207 8.1.3.5 long form length octets */
+    lenleft = b1 & 0x7F;
+    if (lenleft > sigend - *sigp) {
+        return -1;
+    }
+    if (**sigp == 0) {
+        /* Not the shortest possible length encoding. */
+        return -1;
+    }
+    if ((size_t)lenleft > sizeof(size_t)) {
+        /* The resulting length would exceed the range of a size_t, so
+         * certainly longer than the passed array size.
+         */
+        return -1;
+    }
+    while (lenleft > 0) {
+        ret = (ret << 8) | **sigp;
+        if (ret + lenleft > (size_t)(sigend - *sigp)) {
+            /* Result exceeds the length of the passed array. */
+            return -1;
+        }
+        (*sigp)++;
+        lenleft--;
+    }
+    if (ret < 128) {
+        /* Not the shortest possible length encoding. */
+        return -1;
+    }
+    return ret;
+}
+
+static int secp256k1_der_parse_integer(char *r, const unsigned char **sig, const unsigned char *sigend) {
+    int overflow = 0;
+    unsigned char ra[32] = {0};
+    int rlen;
+    
+    if (*sig == sigend || **sig != 0x02) {
+        /* Not a primitive integer (X.690-0207 8.3.1). */
+        return 0;
+    }
+    (*sig)++;
+    rlen = secp256k1_der_read_len(sig, sigend);
+    if (rlen <= 0 || (*sig) + rlen > sigend) {
+        /* Exceeds bounds or not at least length 1 (X.690-0207 8.3.1).  */
+        return 0;
+    }
+    if (**sig == 0x00 && rlen > 1 && (((*sig)[1]) & 0x80) == 0x00) {
+        /* Excessive 0x00 padding. */
+        return 0;
+    }
+    if (**sig == 0xFF && rlen > 1 && (((*sig)[1]) & 0x80) == 0x80) {
+        /* Excessive 0xFF padding. */
+        return 0;
+    }
+    if ((**sig & 0x80) == 0x80) {
+        /* Negative. */
+        overflow = 1;
+    }
+    while (rlen > 0 && **sig == 0) {
+        /* Skip leading zero bytes */
+        rlen--;
+        (*sig)++;
+    }
+    if (rlen > 32) {
+        overflow = 1;
+    }
+    if (!overflow) {
+        memcpy(ra + 32 - rlen, *sig, rlen);
+        
+        //secp256k1_scalar_set_b32(r, ra, &overflow);
+    }
+    if (overflow) {
+        //secp256k1_scalar_set_int(r, 0);
+    }
+    (*sig) += rlen;
+    
+    memcpy(r,ra,rlen);
+    return 1;
+}
+
+static int der_sig_parse(char *rr, char *rs, const unsigned char *sig, size_t size) {
+    const unsigned char *sigend = sig + size;
+    int rlen;
+    if (sig == sigend || *(sig++) != 0x30) {
+        /* The encoding doesn't start with a constructed sequence (X.690-0207 8.9.1). */
+        return 0;
+    }
+    rlen = secp256k1_der_read_len(&sig, sigend);
+    if (rlen < 0 || sig + rlen > sigend) {
+        /* Tuple exceeds bounds */
+        return 0;
+    }
+    if (sig + rlen != sigend) {
+        /* Garbage after tuple. */
+        return 0;
+    }
+    
+    if (!secp256k1_der_parse_integer(rr, &sig, sigend)) {
+        return 0;
+    }
+    if (!secp256k1_der_parse_integer(rs, &sig, sigend)) {
+        return 0;
+    }
+    
+    if (sig != sigend) {
+        /* Trailing garbage inside tuple. */
+        return 0;
+    }
+    
+    return 1;
+}
 
 @implementation NSData (NSData_hexadecimalString)
 
